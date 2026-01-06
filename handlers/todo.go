18f034cd242bf592b48e63c33c3545e139d1029c
Guru Sanjay <gurucoolmsd@gmail.com>
2026-01-06 15:56:57 +0530

    Created Get Endpoint and get by id

--- handlers/todo.go
+++ handlers/todo.go
@@ -3,10 +3,30 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 	"todo-api/models"
 )
 var todos []models.Todo
 var nextID = 1
+func Todoshandler(w http.ResponseWriter , r *http.Request){
+	if r.URL.Path == "/todos" {
+	switch r.Method {
+	case http.MethodPost:
+		CreateTodo(w,r)
+	case http.MethodGet:
+		GetTodos(w,r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+	return
+	}
+	if strings.HasPrefix(r.URL.Path,"/todos/"){
+		GetTodoByID(w,r)
+		return
+	}	
+	w.WriteHeader(http.StatusNotFound)
+}
 func CreateTodo(w http.ResponseWriter , r *http.Request) {
 	if r.Method != http.MethodPost{
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -24,4 +44,35 @@ func CreateTodo(w http.ResponseWriter , r *http.Request) {
 	w.Header().Set("Content-Type","application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(todo)
+	
+}
+func GetTodos(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet{
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(todos)
+
+}
+func GetTodoByID (w http.ResponseWriter , r *http.Request){
+	if r.Method != http.MethodGet{
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	idStr := strings.TrimPrefix(r.URL.Path,"/todos/")
+	id , err := strconv.Atoi(idStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	for _, todo := range todos {
+		if todo.ID == id {
+			w.Header().Set("Content-Type","application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(todo)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
 }
--- main.go
+++ main.go
@@ -1,4 +1,9 @@
 package main
+import (
+	"todo-api/handlers"
+	"net/http"
+)
 func main(){
-	
+	http.HandleFunc("/todos",handlers.Todoshandler)
+	http.ListenAndServe(":8080",nil)
 }
